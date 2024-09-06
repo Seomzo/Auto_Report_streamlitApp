206e6487472ddf60d62668677d921a35e3c58bfd
Seomzo <128646632+Seomzo@users.noreply.github.com>
2024-09-05 21:49:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,7 +117,7 @@ def process_alacarte_data(df, names_column):
     return name_counts, labor_gross_sums, parts_gross_sums
 
 def process_commodities_data(df, names_column="Primary Advisor Name"):
-    st.write("Available columns in Commodities data:", df.columns.tolist())
+    #st.write("Available columns in Commodities data:", df.columns.tolist())
     df.columns = df.columns.str.strip()
     if names_column not in df.columns:
         st.error(f"Column '{names_column}' not found in the uploaded Commodities Excel. Please check the column names.")
@@ -134,7 +134,7 @@ def process_commodities_data(df, names_column="Primary Advisor Name"):
 
 def process_recommendations_data(df, names_column="Name"):
     # Display available columns for debugging
-    st.write("Available columns in Recommendations data:", df.columns.tolist())
+    #st.write("Available columns in Recommendations data:", df.columns.tolist())
     
     # Ensure no leading/trailing spaces in column names
     df.columns = df.columns.str.strip()
@@ -168,6 +168,7 @@ def process_recommendations_data(df, names_column="Name"):
 
 def update_google_sheet(sheet, name_counts, *args, date, start_row, handle_two_outputs=False):
     headers = sheet.row_values(2)  # Assuming the date is in row 2
+    date = date.lstrip('0')
     if date in headers:
         date_column_index = headers.index(date) + 1
     else:
